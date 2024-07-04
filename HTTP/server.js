2c4740628699b1dafaaec80a5840c5572bae9ba0
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2024-07-04 10:49:40 +0530

    tst

---
+++ HTTP/server.js
@@ -0,0 +1,9 @@
+const http =require('http')
+let server=http.createServer((req,resp)=>{
+    resp.end("<h1>Hello,GM......</h1>")
+})
+
+server.listen(8080,'127.0.0.1',(err)=>{
+        if(err) throw err 
+        console.log("Server is Running")
+})
