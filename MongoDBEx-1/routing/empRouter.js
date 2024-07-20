b1fd7b5af40a256296cf7d5a1e08342818f249f2
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2024-07-20 10:32:26 +0530

    Tst

--- MongoDBEx-1/routing/empRouter.js
+++ MongoDBEx-1/routing/empRouter.js
@@ -45,4 +45,54 @@ router.post("/create",async (req,resp)=>{
     }
        
 })
+
+/*
+    usage:update employee Data
+    URL:http://127.0.0.1:8080/emp/update/1
+    Method:PUT
+    Access Tupe:Public
+    Req Fields: eid,ename,esal
+*/
+router.put("/update/:id",async(req,resp)=>{
+    try{
+            let emp_Id = req.params.id;
+            console.log(emp_Id)
+            let emp=req.body;
+            console.log(emp)
+            let Emplooye=await EmployeeModel.findOne({"eid":emp_Id})
+            console.log(Emplooye)
+            if(!Emplooye){
+                    return resp.status(401).json({"msg":"Employee Not Exits"})
+            }
+            await EmployeeModel.findByIdAndUpdate(Emplooye._id,{$set:{ename:emp.ename,esal:emp.esal}});
+            return resp.status(200).json({"msg":"Employee Updated Successfully"})
+    }
+    catch(err){
+        return resp.status(401).json({"err":err.message})
+    }
+})
+/*
+    usage:Delete employee Data
+    URL:http://127.0.0.1:8080/emp/delete/10
+    Method:DELETE
+    Access Tupe:Public
+    Req Fields: None
+*/
+
+router.delete("/delete/:id",async(req,resp)=>{
+    try{
+      let emp_Id = req.params.id;
+      console.log(emp_Id)
+      let Employee  =await EmployeeModel.findOne({eid:emp_Id})
+      console.log(Employee)
+      if(!Employee){
+        return resp.status(401).json({"msg":"Employee Not Exits"})
+      }
+      await EmployeeModel.findByIdAndDelete(Employee._id);
+      return resp.status(200).json({"msg":"Employee document deleted"})
+    }
+    catch(err){
+        return resp.status(401).json({"err":err.message})
+    }
+})
 export default router;
